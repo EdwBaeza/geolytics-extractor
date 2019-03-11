28d557b0d1953c7c34eccfaa003a5a9133a16853
Luis Rogel <luisfrr27@gmail.com>
2019-03-11 11:34:14 -0600

    Default Mapper Modified

--- general/Mapper.py
+++ general/Mapper.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
-from geolytics_connector.models import GeoModel
+from .geomodel import GeoModel
+import json
 
 class AbstractMapper(object):
     __metaclass__ = ABCMeta
@@ -15,8 +16,8 @@ class AbstractMapper(object):
 
 class DefaultMapper(AbstractMapper):
     
-
     def mapout_API(self, obj_json, common_data, metadata):
+        
         copy_metadata = {}
         objs_list = []
 
@@ -36,18 +37,17 @@ class DefaultMapper(AbstractMapper):
                 else:
                     del copy_metadata[key]
 
-            print("")
-            print("GEOMODEL:[point: Point({0},{1}), data: {2}, metadata: {3}]".format(longitude,latitude,data,copy_metadata))
-            print("")
+            #creamos un objeto GeoModel
+            model = GeoModel(data,copy_metadata,latitude,longitude)
 
-            #model = GeoModel(longitude, latitude, data, metadata)
-            #model = GeoModel(data,metadata,latitude,longitude)
-
-            #objs_list.append(model)
+            # __dict__ -> Convierte el objeto a diccionario
+            # y se agrega a la lista de objectos GeoModel
+            objs_list.append(model.__dict__)
 
             copy_metadata.clear()
-        
-        #return json.dumps(objs_list)
 
-    def  mapout_DB_Unstructed(self,data_not_mapped):
+        #Retornamos la lista de objetos GeoModel en formato Json
+        return json.dumps(objs_list, indent=4)
+
+    def  mapout_DB_Unstructed(self, data_not_mapped):
         pass
